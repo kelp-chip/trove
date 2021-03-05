da8e6f2603efe55fe2973c92f50fd1bd52ec580f
joelsrubin <joelsrubin@gmail.com>
2021-03-05 15:21:36 -0600

    .env switched to local

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+  useEffect(() => {
+    fetch('http://localhost:3000/api/collections')
+      .then(result => result.json())
+      .then(data => console.log(data))
+      .catch(err => console.error(err));
+  });
   return (
     <div>
 
     </div>
-  )
+  );
 }
