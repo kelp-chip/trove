0ca9b680a0e52f5363dafc3af91a40e4b29f7cf6
Lexy Kramer <lexyk@Lexys-MBP.attlocal.net>
2021-03-18 13:58:48 -0400

    Initialized basic display pages

---
+++ pages/collections/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function Collections() {
+    
+
+  return (
+    <div>
+       <h1>Collections Page</h1>
+    </div>
+  );
+}
+
+
+//Route to access this page
+//localhost:3000/collections
--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
   });
   return (
     <div>
-
+      app
     </div>
   );
 }
