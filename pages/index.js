96d589ddc061c99a213e971a14f80b1152767e81
joelsrubin <joelsrubin@gmail.com>
2021-03-19 11:31:15 -0500

    dark and light mode style sheets added with styling for edit, close, save, cancel, delete, and addField buttons; theme selector button and logic added as well to switch between light and dark mode

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,47 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import dark from '../styles/base-dark.module.css';
+import light from '../styles/base-light.module.css';
 import { useEffect, useState } from 'react';
 import Layout from '../components/layout';
 
 
 export default function Home() {
   const [data, setData] = useState(null);
+  const [theme, setTheme] = useState(light);
+
   useEffect(() => {
     fetch('http://localhost:3000/api/collections')
       .then(result => result.json())
       .then(data => setData(data))
       .catch(err => console.error(err));
   }, []);
-  return (
 
-    <div>
-    <Layout>
-      <div>
+  const click = () => {
+    theme === dark ? setTheme(light) : setTheme(dark);
+  };
+
+  return (
+    <>
+      <Layout>
         <div>
-          Players:{" "}
-          {data ? data.map((input) => (
-            input.player
+          <h1>Collections:{" "}</h1>
+          {data ? data[0].collections.map((input) => (
+            <div key={input.id}>{input.id}</div>
           )) : <span>Loading...</span>}
         </div>
-      </div>
-    </Layout>
-  </div>
+        <div className={styles.buttonContainer}>
+          <button onClick={click}>theme changer</button>
+          Buttons
+          <button className={theme.editBtn}>edit item</button>
+          <button className={theme.closeBtn}>close item</button>
+          <button className={theme.saveBtn}>save item</button>
+          <button className={theme.cancelBtn}>cancel</button>
+          <button className={theme.deleteBtn}>delete collection</button>
+          <button className={theme.addFieldBtn}>+ add a field</button>
+        </div>
+      </Layout>
+    </>
   );
 }
+
