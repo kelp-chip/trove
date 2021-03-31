02a99aeb1c08951c3a7c20f81400545990c44546
joelsrubin <joelsrubin@gmail.com>
2021-03-31 09:39:26 -0500

    changes

--- pages/collections/[collection_id]/[item_id].js
+++ pages/collections/[collection_id]/[item_id].js
@@ -1,11 +1,48 @@
 import Head from 'next/head';
 import { useAppContext } from '../appContext.js';
 
-export default function Collections() {
+export const getStaticPaths = async () => {
+  let user = 1;
+  const collections = await fetch(`http://localhost:3000/api/collections?id=${user}`)
+    .then(result => result.json());
+  // fetch collections from individual users
+  const paths = collections.map((collection) => {
+    return {
+      params: {
+        id: collection.id.toString()
+      }
+    };
+  });
 
+  return {
+    paths: paths,
+    fallback: false
+  };
+};
+
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  console.log("context: ", context);
+  // fetch items from individual collections
+  const items = await fetch(`http://localhost:3000/api/collections?id=${id}`)
+    .then(result => result.json());
+
+
+  return {
+    props: {
+      collection: items
+    }
+  };
+};
+
+
+
+
+export default function Collections({ collection }) {
+  console.log(collection);
   return (
     <div>
-      <h1>Item Page</h1>
+      <h1>{collection.id} Page</h1>
     </div>
   );
 }
